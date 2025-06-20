afcb4350061a40953e9b5b81817da98b04cca595
gabrielliman <gabrielliman2002@gmail.com>
2025-06-20 17:56:24 -0300

    start notebook

--- dataPrep/tokenizer.py
+++ dataPrep/tokenizer.py
@@ -14,14 +14,22 @@ def main():
     tokenizer = AutoTokenizer.from_pretrained(args.tokenizer)
 
     # Load data
-    df = pd.read_csv(args.input)
+    df = pd.read_csv(args.input, low_memory=False)
 
     if args.text_column not in df.columns:
         raise ValueError(f"Column '{args.text_column}' not found in input file.")
 
     # Tokenize
+    # def tokenize_text(text):
+    #     return tokenizer.encode(text, add_special_tokens=True)
     def tokenize_text(text):
-        return tokenizer.encode(text, add_special_tokens=True)
+        return tokenizer.encode(
+            text,
+            add_special_tokens=True,
+            truncation=True,
+            max_length=512
+        )
+
 
     df[f"{args.text_column}_tokens"] = df[args.text_column].astype(str).apply(tokenize_text)
 
