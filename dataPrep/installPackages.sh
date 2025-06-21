b2faa623f88d07aafcf5324d7ebea1b75dd67f96
Marco Túlio P. T. Tristão <marcotuliopin@gmail.com>
2025-06-21 15:12:51 -0300

    Add installPackages.sh script for conda package installation

---
+++ dataPrep/installPackages.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# If you'd rather use conda to install the packages, but don't want to copy a conda environment file,
+# you may use this script to install the required packages.
+
+conda install pandas scikit-learn pytorch torchvision torchaudio pytorch-cuda=11.8 transformers opacus ipykernel numpy -c pytorch -c nvidia -c huggingface -c conda-forge -y
