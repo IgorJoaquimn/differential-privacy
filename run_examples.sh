2ce5fae20e77353b2458dac6108bc2f36e34429f
Marco Túlio P. T. Tristão <marcotuliopin@gmail.com>
2025-06-22 16:34:55 -0300

    Update run_examples.sh to include checkpoint paths for model testing

--- run_examples.sh
+++ run_examples.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-python pipelines.py --model tem # Train the TEM model
+python pipelines.py --model tem # Train and test the TEM model
 
-python pipelines.py --model tem --eval <checkpoint_path> # Test the TEM model with a checkpoint
+python pipelines.py --model tem --eval checkpoints/tem.pt # Only test the TEM model with a checkpoint
 
-python pipelines.py --model madlib --eval <checkpoint_path> # Test the MADlib model with a checkpoint
+python pipelines.py --model madlib --eval checkpoints/madlib.pt # Test the MADlib model with a checkpoint
 
 python pipelines.py --run_private # Train the baseline model with DP-SGD
+
+python pipelines.py --run_private --eval checkpoints/baseline_private.pt # Test the baseline model with DP-SGD and a checkpoint
