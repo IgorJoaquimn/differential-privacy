fix: correct normalization of original_embedding in NoisyEmbedding class

models/tem.py
@@ -54,7 +54,7 @@ class NoisyEmbedding(nn.Module):
     def __init__(self, original_embedding, epsilon=0.1):
         super().__init__()
         self.embedding = original_embedding
-        self.normalized_embedding = F.normalize(original_embedding, p=2, dim=1).weight           # (V, D)
+        self.normalized_embedding = F.normalize(original_embedding.weight, p=2, dim=1)           # (V, D)
         self.epsilon = epsilon
 
     def forward(self, input_ids):